dd2b47f1eb7dc764242a5f4223a0e15adc3f71ec
Fleli <frederikedvardsen@gmail.com>
2023-10-13 11:02:50 +0200

    Fixed issue where Swift keywords were un-escaped twice, producing syntactically incorrect code.

--- Sources/SwiftParse/Type Generation/build_class.swift
+++ Sources/SwiftParse/Type Generation/build_class.swift
@@ -44,7 +44,7 @@ extension Generator {
             
             let initLines = allFields
                 .map { (name: $0.name.nonColliding, isNil: !chosenFields.contains($0)) }
-                .map { "\t\tself.\($0.name.nonColliding) = \($0.isNil ? "nil" : $0.name.nonColliding)\n" }
+                .map { "\t\tself.\($0.name) = \($0.isNil ? "nil" : $0.name)\n" }
                 .reduce("") { $0 + $1 }
             
             return "\t\n\tinit(\(signatureArgumentList)) {\n\(initLines)\t}\n"
@@ -143,7 +143,7 @@ extension Generator {
             
         } else if let deciding = variables.first {
             
-            return "(\(deciding.name) == nil ? \"\" : " + element.classItems.map {$0.inDescriptor(true)}.reduce("", {$0 + " + " + $1}).dropFirst(3) + ")"
+            return "(\(deciding.name) == nil ? \"\" : " + element.classItems.map {$0.inDescriptor(true)}.reduce("", {$0 + " + " + $1.nonColliding}).dropFirst(3) + ")"
             
         }
         
