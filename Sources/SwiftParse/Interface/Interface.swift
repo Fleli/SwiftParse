ae5c7c7d1e6b9fd9368fbc8669e5592147559441
Fleli <frederikedvardsen@gmail.com>
2023-11-02 15:37:43 +0100

    Added the option to write the grammar to a file.

--- Sources/SwiftParse/Interface/Interface.swift
+++ Sources/SwiftParse/Interface/Interface.swift
@@ -1,6 +1,10 @@
-public func generateFiles(specification: String, path: String, visibility: SwiftVisibility, typeFileOption: FileOption) throws {
+
+public func generateFiles(specification: String, path: String, visibility: SwiftVisibility, typeFileOption: FileOption, grammarFile: String?) throws {
+    
     let visibilityKeyword = visibility.rawValue
     let typePath = typeFileOption.typePath
+    
     let generator = Generator()
-    try generator.createParser(from: specification, at: path, visibility: visibilityKeyword, typePath: typePath)
+    try generator.createParser(from: specification, at: path, visibility: visibilityKeyword, typePath: typePath, grammarFile: grammarFile)
+    
 }
